275a1963b4d5470d8499445327ca1a8b19675c63
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-23 15:06:00 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// dependencies
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const routes = require("./controllers");
+const exphbs = require("express-handlebars");
 
+// middleware
 const app = express();
 const PORT = process.env.PORT || 3001;
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const hbs = exphbs.create({ helpers });
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+// establish session rules
+const sess = {
+  secret: "secretSquirrel",
+  cookie: {
+    // end session on idle for 10 minutes
+    expires: 10 * 60 * 1000,
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// set express rules
+app.use(routes);
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+// set handlebar connection
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
+// connect database with server via Sequelize
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Rolling deep on port ${PORT}!!`));
 });
