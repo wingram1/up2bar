8935019088c185d8b292791286b89397dd17c452
Laertes <maclellanj15@gmail.com>
2022-03-24 16:59:30 -0500

    forgot to checkout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,7 +5,7 @@ const withAuth = require("../../utils/auth");
 // get all users
 router.get("/", (req, res) => {
   User.findAll({
-    attributes: { exclude: ["password"] },
+    // attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,7 +5,7 @@ const withAuth = require("../utils/auth");
 
 // get function for all posts that meet rules
 router.get("/", (req, res) => {
-  // res.render('dashboard');
+  
   Post.findAll({
     where: {
       user_id: req.session.user_id,
