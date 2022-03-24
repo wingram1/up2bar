9c7fc324cbbfc7d34a21a78a39d419059609a270
Laertes <maclellanj15@gmail.com>
2022-03-23 19:31:59 -0500

    minor error fix

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -23,7 +23,7 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Post,
-        attributes: ["id", "title", "post_url", "created_at"],
+        attributes: ["id", "title", "post_content", "created_at"],
       },
       {
         model: Comment,
@@ -32,13 +32,7 @@ router.get("/:id", (req, res) => {
           model: Post,
           attributes: ["title"],
         },
-      },
-      {
-        model: Post,
-        attributes: ["title"],
-        through: Vote,
-        as: "voted_posts",
-      },
+      }
     ],
   })
     .then((dbUserData) => {
